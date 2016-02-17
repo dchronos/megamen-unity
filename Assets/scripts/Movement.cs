c592f53ed201f6fd4fea8406610d912d1f6347fc
Luiz Carvalho <projetodc@gmail.com>
2016-02-16 22:23:24 -0200

    Add jump

--- Assets/scripts/Movement.cs
+++ Assets/scripts/Movement.cs
@@ -3,6 +3,12 @@ using System.Collections;
 
 public class Movement : MonoBehaviour {
 
+	public Animator anim;
+	float velocity = 0;
+	float jump = 0;
+	public SpriteRenderer srender;
+	public Rigidbody2D rdb;
+
 	// Use this for initialization
 	void Start () {
 	
@@ -10,6 +16,20 @@ public class Movement : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-	
+		velocity = Input.GetAxis ("Horizontal");
+		jump = Input.GetAxis ("Vertical");
+
+		anim.SetFloat ("vel", velocity);
+		if (velocity > 0)
+			srender.flipX = false;
+		if (velocity < 0)
+			srender.flipX = true;
+		anim.SetFloat ("vel", Mathf.Abs(velocity));
+	}
+
+	void FixedUpdate () {
+		//rdb.AddForce (Vector2.right * velocity * (14 - Mathf.Abs (rdb.velocity.x)));
+		rdb.velocity = new Vector2 (velocity*4, jump*4);
+		Debug.Log (jump);
 	}
 }
